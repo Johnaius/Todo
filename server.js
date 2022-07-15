d141e05b61ffeed1aa6fac99ad786a331f46b1cf
unknown <johnzak80@gmail.com>
2022-07-15 18:27:17 -0400

    throwing spaghetti at wall

--- server.js
+++ server.js
@@ -25,12 +25,9 @@ app.get('/', async (req, res) => {
             res.render ('index.ejs', {todoTasks : tasks})
         })
     }catch (err) {
-
-
+        if (err) return res.status(500).send(err)
     }
-}
-
-)
+})
 
 //Setting up port
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
